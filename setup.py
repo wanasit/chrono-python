065d4aef8502ac86cb3cd8a3eac960368ffdd273
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2014-06-07 13:58:24 +0900

    Change setup content

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='TowelStuff',
-    version='0.1.0',
-    author='J. Random Hacker',
-    author_email='jrh@example.com',
-    packages=['towelstuff', 'towelstuff.test'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    name='chrono',
+    description='A natural language date parser',
+    version='0.0.1',
+    author='Wanasit Tanakitrungruang',
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
+    packages=['chrono'],
+    url='https://github.com/wanasit/chrono-python',
+    download_url='https://github.com/wanasit/chrono-python/tarball/0.0.1',
+    keywords = ['parser', 'time', 'date', 'natural'],
+    install_requires=[]
 )
