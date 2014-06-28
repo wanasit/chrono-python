d5757c6604e57839f9be060ca560d9c5439b0e45
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2014-06-28 18:59:52 +0900

    Add Japanese standard date parser

---
+++ chrono/parsers/jp/standard_parser.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import re
+import util
+
+from ..parser import Parser
+from ..parser import ParsedResult
+from ..parser import ParsedComponent
+
+from datetime import datetime
+from util import date_exist
+from util import find_closest_year
+
+class JPStandartDateFormatParser(Parser):
+
+    def pattern(self):
+        return '(((平成|昭和)?([0-9０-９]{2,4})年|今年|去年|来年)|[^年]|^)([0-9０-９]{1,2}|今|先|来)月([0-9０-９]{1,2})日\s*(?:\((?:日|月|火|水|木|金|土)\))?'
+
+    def extract(self, text, ref_date, match, options):
+
+        result = ParsedResult()
+        result.index = match.start()
+        result.text  = match.group(0)
+
+        day   = int(match.group(6))
+        month = ref_date.month
+        if match.group(5) == '先':
+            month -= 1
+        elif match.group(5) == '来':
+            month += 1
+        elif match.group(5) != '今':
+            month = int(match.group(5))
+
+        year = None
+        if match.group(4):
+            year = int(match.group(4))
+
+            if match.group(3) == '平成':
+                year += 1989
+            elif match.group(3) == '昭和':
+                year += 1926
+        else:
+
+            if match.group(5) == '今年':
+                year = ref_date.year
+            elif match.group(5) == '去年':
+                year = ref_date.year - 1
+            elif match.group(5) == '来年':
+                year = ref_date.year + 1
+            else:
+                result.index += len(match.group(1))
+                result.text = result.text[len(match.group(1)):]
+
+
+        result.start = ParsedComponent(month=month, day=day)
+        if year:
+            if not date_exist(year, month, day): return None
+            result.start.assign('year', year)
+        else:
+            year = find_closest_year(ref_date=ref_date, month=month, day=day)
+            if year is None: return None
+
+            result.start.imply('year', year)
+        
+        return result
---
+++ chrono/parsers/jp/util.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import re
+import datetime
+
+MONTH_NAMES = {
+    "january":1,
+    "jan": 1,
+    "february": 2,
+    "feb": 2,
+    "march": 3,
+    "mar": 3,
+    "april": 4,
+    "apr": 4,
+    "may": 5,
+    "june": 6,
+    "jun": 6,
+    "july": 7,
+    "jul": 7,
+    "august": 8,
+    "aug": 8,
+    "september": 9,
+    "sep": 9,
+    "october": 10,
+    "oct": 10,
+    "november": 11,
+    "nov": 11,
+    "december": 12,
+    "dec": 12
+}
+
+def month_index(month_name):
+    return MONTH_NAMES[month_name.lower()]
+
+def date_exist(year, month, day):
+    try:
+        return datetime.date(year, month, day).day == day
+    except ValueError:
+        return False
+
+def find_closest_year(ref_date, month, day):
+
+    year = None
+
+    if date_exist(ref_date.year, month, day):
+        year = ref_date.year
+
+    if date_exist(ref_date.year-1, month, day):
+        if year:
+            tdelta1 = datetime.datetime(year, month, day) - ref_date
+            tdelta2 = datetime.datetime(ref_date.year-1, month, day) - ref_date
+            if abs(tdelta2) < abs(tdelta1):
+                year = ref_date.year-1
+        else:
+            year = ref_date.year-1
+
+    if date_exist(ref_date.year+1, month, day):
+        if year:
+            tdelta1 = datetime.datetime(year, month, day) - ref_date
+            tdelta2 = datetime.datetime(ref_date.year+1, month, day) - ref_date
+            if abs(tdelta2) < abs(tdelta1):
+                year = ref_date.year+1
+        else:
+            year = ref_date.year+1
+
+    return year
